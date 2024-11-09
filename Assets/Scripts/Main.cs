f26c7c8ae0559e559d2eb9ea1daa4d1b0136692b
Rain <wl20000718@gmail.com>
2024-11-09 23:04:37 +0800

    enh : support MR

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.XR.PXR;
+
+public class Main : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PXR_Manager.EnableVideoSeeThrough = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
